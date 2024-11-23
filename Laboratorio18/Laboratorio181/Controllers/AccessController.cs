d48b1ec7e3c941eba2e5e90208bd4c7988014a6d
Rolando Flores <rafj26@outlook.com>
2024-11-22 23:16:15 -0500

    Laboratorio completado

---
+++ Laboratorio18/Laboratorio181/Controllers/AccessController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio181.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(String user, String password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error :(" + ex.Message);
+            }
+        }
+    }
+}
